6663bfffacfde0e2ae9155a8e0a60366dedf6ad5
adityapatil895-cyber <lucifermorningsta895@gmil.com>
2026-02-18 14:40:12 +0530

    test code

---
+++ LEC_7/table.js
@@ -0,0 +1,24 @@
+// for(let i=1;i<11;i++){
+//     console.log(11*i)
+// }
+
+// for(let i=1;i<=50;i++)
+//     {
+//     console.log(i*i)
+// }
+
+// for(let i=1;i<=25;i++){
+//     console.log(Math.sqrt(i))
+// }
+
+
+let count = 10;
+let i = 0;
+let sum = 0;
+while(count!=0){
+    if(i%2==0){
+        sum += i;
+        count--;
+    }
+    i++
+    }
